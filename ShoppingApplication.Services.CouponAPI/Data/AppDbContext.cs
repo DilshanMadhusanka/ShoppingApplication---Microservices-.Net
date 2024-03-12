6f88668ef090e35155a55ec828c3634f5048df46
DilshanMadhusanka <139458721+DilshanMadhusanka@users.noreply.github.com>
2024-03-12 22:37:45 +0530

    Add project files.

---
+++ ShoppingApplication.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingApplication.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ ShoppingApplication.Services.CouponAPI/Data/Dbcontext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingApplication.Services.CouponAPI.Data
+{
+    public class Dbcontext
+    {
+        private DbContextOptions<AppDbContext> options;
+
+        public Dbcontext(DbContextOptions<AppDbContext> options)
+        {
+            this.options = options;
+        }
+    }
+}
