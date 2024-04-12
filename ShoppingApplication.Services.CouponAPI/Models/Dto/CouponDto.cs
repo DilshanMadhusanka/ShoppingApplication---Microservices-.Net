0ef2b3a5b0020883540eb08c1268d310d2894722
DilshanMadhusanka <139458721+DilshanMadhusanka@users.noreply.github.com>
2024-04-12 23:08:38 +0530

    update

--- ShoppingApplication.Services.CouponAPI/Models/Dto/CouponDto.cs
+++ ShoppingApplication.Services.CouponAPI/Models/Dto/CouponDto.cs
@@ -1,11 +1,13 @@
 ﻿namespace ShoppingApplication.Services.CouponAPI.Models.Dto
 {
-    // front end ekat dena attributes tika 
+    // front end ekat dena attributes tika dan class eka 
     public class CouponDto
     {
         public int CouponId { get; set; }
         public string CouponCode { get; set; }
         public double DiscountAmount { get; set; }
         public int MinAmount { get; set; }
+
+
     }
 }
